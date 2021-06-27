f7fe129407b49451d46d214183283caee585605e
Kyle Cho <kylejcho@gmail.com>
2021-06-27 16:32:47 -0400

    1 Round setup

---
+++ app.js
@@ -0,0 +1,44 @@
+function computerPlay() {
+    //returns random integer from 0-2.
+    let randNum = Math.floor(Math.random() * 3);
+    if (randNum === 0) {
+        return "rock";
+    } else if (randNum === 1) {
+        return "paper";
+    } else {
+        return "scissors";
+    }
+}
+
+
+const playerSelection = "rock";
+
+function playRound(playerSelection, computerSelection) {
+    if (playerSelection == "rock" && computerSelection == "rock") {
+        return ("It's a tie!");
+    } else if (playerSelection == "rock" && computerSelection == "scissors") {
+        return ("You win! Rock beats scissors.");
+    } else if (playerSelection == "rock" && computerSelection == "paper"){
+        return ("You lose! Paper beats rocks.");
+    } else if (playerSelection == "paper" && computerSelection == "scissors"){
+        return ("You lose! Scissors beats rocks.");
+    } else if (playerSelection == "paper" && computerSelection == "paper") {
+        return ("It's a tie");
+    } else if (playerSelection == "paper" && computerSelection == "rock") {
+        return ("You Win! Paper beats rocks.");
+    } else if (playerSelection == "scissors" && computerSelection == "rock") {
+        return ("You lose! Rocks beat scissors.");
+    } else if (playerSelection == "scissors" && computerSelection == "paper") {
+        return ("You win! Paper beats scissors."); 
+    } else if (playerSelection == "scissors" && computerSelection == "scissors") {
+        return ("It's a tie!"); 
+    }
+  }
+
+const computerSelection = computerPlay();
+
+
+
+
+
+console.log(playRound(playerSelection, computerSelection));
